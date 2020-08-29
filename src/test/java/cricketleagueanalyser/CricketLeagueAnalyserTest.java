b567fda89fbf8ccc6a95a7842ace930bb956bd01
“teji1998” <tejasvinirpk@gmail.com>
2020-08-29 08:04:47 +0530

    Refactored:Added the before test block to handle DRY principle in test cases

--- src/test/java/cricketleagueanalyser/CricketLeagueAnalyserTest.java
+++ src/test/java/cricketleagueanalyser/CricketLeagueAnalyserTest.java
@@ -2,16 +2,24 @@ package cricketleagueanalyser;
 
 import com.google.gson.Gson;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CricketLeagueAnalyserTest {
+
+	public CricketLeagueAnalyser cricketLeagueAnalyser;
+
 	private static final String IPL_MOST_RUN_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
 	private static final String IPL_MOST_WICKETS_CSV_FILE_PATH = "./src/test/resources/IPL2019FactsheetMostWickets.csv";
+
+	@Before
+	public void initialSetUp() {
+		cricketLeagueAnalyser = new CricketLeagueAnalyser();
+	}
 
 	//Test case to check number of record in IPL most runs CSV file
 	@Test
 	public void givenIPLMostRunsCSVFile_whenCheckedForRecords_shouldReturnCorrectRecords() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		int numberOfRecords = cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		Assert.assertEquals(100,numberOfRecords);
 	}
@@ -19,7 +27,6 @@ public class CricketLeagueAnalyserTest {
 	//Test case to check number of record in IPL most wickets CSV file
 	@Test
 	public void givenIPLMostWicketsCSVFile_whenCheckedForRecords_shouldReturnCorrectRecords() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		int numOfRecords = cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		Assert.assertEquals(99,numOfRecords);
 	}
@@ -27,7 +34,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the Batting average in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByBattingAverage_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBattingAverageWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -37,7 +43,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the Strike Rate in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByStrikeRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getStrikeRateWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -47,7 +52,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the maximum sixes and fours in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedBySixesAndFours_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getMaximumSixesAndFoursWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -57,7 +61,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the maximum sixes and fours with top striking rate in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedBySixesAndFoursWithStrikingRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getMaximumSixesAndFoursWithBestStrikingRateWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -67,7 +70,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top striking rate with great average in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByBestStrikingRateWithGreatAverage_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getGreatAverageWithBestStrikingRateWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -77,7 +79,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the maximum runs with great average in descending order
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByMaximumRunsWithGreatAverage_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getMaximumRunsWithBestAverageWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -87,7 +88,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top bowling average in descending order
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByBowlingAverage_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBowlingAverageWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -97,7 +97,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top bowling striking rate in descending order
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByStrikingRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBowlerStrikingRateWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -107,7 +106,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top bowling economy rate in descending order
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByEconomyRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBowlerEconomyRateWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -117,7 +115,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top bowler having four and five wickets with best striking rates
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByFourAndFiveWicketsWithBestStrikingRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getFourWicketsAndFiveWicketsWithBestStrikingRateWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -127,7 +124,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the top bowler great average with best striking rates
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByAverageWithBestStrikingRate_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBowlersHavingGreatAverageWithBestStrikingRateWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -137,7 +133,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding the bowler with maximum wickets with great average rates
 	@Test
 	public void givenMostWicketsCSVFile_whenSortedByWicketsWithBestAverageWise_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BOWLING, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBowlersTakingMaximumWicketsWithBestBowlingAverageWiseSortedIPLData();
 		IPLMostWicketsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostWicketsCSV[].class);
@@ -147,7 +142,6 @@ public class CricketLeagueAnalyserTest {
 	// Test case for finding cricketer with best bowling average and batting average
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByBowlingAndBattingAverageWise_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getBestBowlerAndBestBatterWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -157,7 +151,6 @@ public class CricketLeagueAnalyserTest {
 	//To find all rounder
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByRunsAndWicketWise_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH, IPL_MOST_WICKETS_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getAllRounderWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -167,7 +160,6 @@ public class CricketLeagueAnalyserTest {
 	//To check cricketers with maximum hundred and best batting average
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByMaximumHundredAndBestBattingAverageWise_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getMaximumHundredAndBattingAverageWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
@@ -177,7 +169,6 @@ public class CricketLeagueAnalyserTest {
 	//To check cricketers with minimum hundreds and fifty and best batting average
 	@Test
 	public void givenMostRunsCSVFile_whenSortedByMinimumHundredsAndFiftyAndBestBattingAverageWise_shouldReturnSortedResult() throws CricketLeagueAnalyserException {
-		CricketLeagueAnalyser cricketLeagueAnalyser = new CricketLeagueAnalyser();
 		cricketLeagueAnalyser.loadLeagueData(IplEnumCollection.CricketerSkill.BATTING, IPL_MOST_RUN_CSV_FILE_PATH);
 		String sortedIPLData = cricketLeagueAnalyser.getMinimumHundredAndMinimumFiftyWithBestBattingAverageWiseSortedIPLData();
 		IPLMostRunsCSV[] iplCSV = new Gson().fromJson(sortedIPLData, IPLMostRunsCSV[].class);
