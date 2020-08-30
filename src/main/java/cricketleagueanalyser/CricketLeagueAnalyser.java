3eefa97f430ff6e7acaf0cc3295397a6d2ecb8ce
“teji1998” <tejasvinirpk@gmail.com>
2020-08-30 16:47:12 +0530

    Refactored:Code to handle single responsibility principle by separating comparator class

--- src/main/java/cricketleagueanalyser/CricketLeagueAnalyser.java
+++ src/main/java/cricketleagueanalyser/CricketLeagueAnalyser.java
@@ -1,7 +1,6 @@
 package cricketleagueanalyser;
 
 import com.google.gson.Gson;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,140 +17,114 @@ public class CricketLeagueAnalyser {
 	}
 
 	public String getBattingAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> battingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(battingAverageComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.battingAverageComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getStrikeRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> battingStrikeRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(battingStrikeRateComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.battingStrikeRateComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getMaximumSixesAndFoursWiseSortedIPLData() {
-		Comparator<LeagueDAO> maxSixOrFourComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.sixes * 6 + iplFactSheet.fours * 4);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(maxSixOrFourComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.sixAndFourComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getMaximumSixesAndFoursWithBestStrikingRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> sixesAndFoursComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.sixes * 6 + iplFactSheet.fours * 4);
-		Comparator<LeagueDAO> strikingRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(sixesAndFoursComparator.thenComparing(strikingRateComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.sixAndFourComparator.thenComparing(IPLComparator.battingStrikeRateComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getGreatAverageWithBestStrikingRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
-		Comparator<LeagueDAO> strikingRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(strikingRateComparator.thenComparing(averageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.battingStrikeRateComparator.thenComparing(IPLComparator.battingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getMaximumRunsWithBestAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
-		Comparator<LeagueDAO> runsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.runs);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(runsComparator.thenComparing(averageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.runsComparator.thenComparing(IPLComparator.battingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBowlingAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> bowlingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(bowlingAverageComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.bowlingAverageComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBowlerEconomyRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> economyComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.economyRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(economyComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.economyComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBowlerStrikingRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> bowlerStrikeRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(bowlerStrikeRateComparator.reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.bowlerStrikeRateComparator.reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getFourWicketsAndFiveWicketsWithBestStrikingRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> fourAndFiveWicketsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.fourWicket + iplFactSheet.fiveWicket);
-		Comparator<LeagueDAO> strikingRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(fourAndFiveWicketsComparator.thenComparing(strikingRateComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.fourAndFiveWicketsComparator.thenComparing(IPLComparator.bowlerStrikeRateComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBowlersHavingGreatAverageWithBestStrikingRateWiseSortedIPLData() {
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingAverage);
-		Comparator<LeagueDAO> strikingRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingStrikeRate);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(averageComparator.thenComparing(strikingRateComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.bowlingAverageComparator.thenComparing(IPLComparator.bowlerStrikeRateComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBestBowlerAndBestBatterWiseSortedIPLData() {
-		Comparator<LeagueDAO> battingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
-		Comparator<LeagueDAO> bowlingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(battingAverageComparator.thenComparing(bowlingAverageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.battingAverageComparator.thenComparing(IPLComparator.bowlingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getBowlersTakingMaximumWicketsWithBestBowlingAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> wicketsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.wicket);
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(wicketsComparator.thenComparing(averageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.wicketsComparator.thenComparing(IPLComparator.bowlingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getAllRounderWiseSortedIPLData() {
-		Comparator<LeagueDAO> wicketsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.wicket);
-		Comparator<LeagueDAO> runsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.runs);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(runsComparator.thenComparing(wicketsComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.runsComparator.thenComparing(IPLComparator.wicketsComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getMaximumHundredAndBattingAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> centuryComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.hundred);
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(centuryComparator.thenComparing(averageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.centuryComparator.thenComparing(IPLComparator.battingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 
 	public String getMinimumHundredAndMinimumFiftyWithBestBattingAverageWiseSortedIPLData() {
-		Comparator<LeagueDAO> minimumHalfCenturyAndCenturyComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.hundred * 100 + iplFactSheet.fifty * 50 == 0);
-		Comparator<LeagueDAO> averageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
 		List<LeagueDAO> leagueDAOList = leagueMap.values().stream()
-				                                             .sorted(minimumHalfCenturyAndCenturyComparator.thenComparing(averageComparator).reversed())
-				                                              .collect(Collectors.toList());
+				  .sorted(IPLComparator.minimumHalfCenturyAndCenturyComparator.thenComparing(IPLComparator.battingAverageComparator).reversed())
+				  .collect(Collectors.toList());
 		return new Gson().toJson(leagueDAOList);
 	}
 }
---
+++ src/main/java/cricketleagueanalyser/IPLComparator.java
@@ -0,0 +1,18 @@
+package cricketleagueanalyser;
+
+import java.util.Comparator;
+
+public class IPLComparator {
+	static Comparator<LeagueDAO> battingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingAverage);
+	static Comparator<LeagueDAO> battingStrikeRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.battingStrikeRate);
+	static Comparator<LeagueDAO> sixAndFourComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.sixes * 6 + iplFactSheet.fours * 4);
+	static Comparator<LeagueDAO> runsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.runs);
+	static Comparator<LeagueDAO> bowlingAverageComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingAverage);
+   static Comparator<LeagueDAO> economyComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.economyRate);
+   static Comparator<LeagueDAO> bowlerStrikeRateComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.bowlingStrikeRate);
+   static Comparator<LeagueDAO> fourAndFiveWicketsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.fourWicket + iplFactSheet.fiveWicket);
+	static Comparator<LeagueDAO> wicketsComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.wicket);
+   static Comparator<LeagueDAO> centuryComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.hundred);
+	static Comparator<LeagueDAO> minimumHalfCenturyAndCenturyComparator = Comparator.comparing(iplFactSheet -> iplFactSheet.hundred * 100 + iplFactSheet.fifty * 50 == 0);
+
+}
